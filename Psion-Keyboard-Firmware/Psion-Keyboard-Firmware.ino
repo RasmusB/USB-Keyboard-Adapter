996b46f0dffa7e94d84d3ff185bc947ac3a98654
RasmusB <RasmusB@users.noreply.github.com>
2018-06-12 20:40:45 +0200

    Almost enables Delete
    Added UK Linux layout

--- Psion-Keyboard-Firmware/Psion-Keyboard-Firmware.ino
+++ Psion-Keyboard-Firmware/Psion-Keyboard-Firmware.ino
@@ -1,91 +1,79 @@
 /*
- Psion-Keyboard-Firmware
- 
- This is firmware for turning a Psion Series 5/5mx or Ericsson MC 128
- keyboard into a USB keyboard. The firmare is intended to run on a 
- ATmega32U4.
- 
- */
+  Psion-Keyboard-Firmware
+
+  See https://github.com/RasmusB/USB-Keyboard-Adapter
+
+  This is firmware for turning a Psion Series 5/5mx or Ericsson MC 128
+  keyboard into a USB keyboard. The firmare is intended to run on a
+  ATmega32U4.
+
+*/
 
 #include "board.h"
 #include "Keyboard.h"
 #include "PsionKeymapUSB.h"
 
-#define SERIAL_ENABLED 0
+#define SERIAL_ENABLED 1
 #define KEYBOARD_ENABLED 1
 
 int keypressArrayCurrent [NROWS] [NCOLS];
 int keypressArrayPrevious [NROWS] [NCOLS];
 
 void setup() {
-  
+
   if (SERIAL_ENABLED) Serial.begin(9600);
-  
-  Keyboard.begin();  
-  
+
+  Keyboard.begin();
+
   // Scanlines in high-Z (inactive) mode
   // Pin state should be initialized to LOW
   for (int i = 0; i < NROWS; i++) {
     pinMode(Rows[i], INPUT);
   }
-  
+
   // Columns as inputs
   // Internal pull-up enabled
   for (int i = 0; i < NCOLS; i++) {
     pinMode(Cols[i], INPUT_PULLUP);
   }
-  
-  /*
-  // Prevoius state = no keys pressed
-  for ( int i=0; i < NROWS; i++ ) {
-    for ( int j=0; j < NCOLS; j++ ) {
-      keypressArrayPrevious[i][j] = 1;
-    }
-  } 
- */ 
 }
 
 void loop() {
-  
-  int nKeysPressed = scanKeyboard(keypressArrayCurrent);
-  
-  /*
-  Serial.print(nKeysPressed);
-  Serial.println(" keys pressed");
-  */
-  
-  sendKeys(keypressArrayCurrent, keypressArrayPrevious);
-  
-  memcpy(keypressArrayPrevious, keypressArrayCurrent, sizeof(keypressArrayCurrent));
-  
-  //delay(1000);
 
+  // Scan the keyboard matrix
+  int nKeysPressed = scanKeyboard(keypressArrayCurrent);
+
+  // Send the keypresses over USB
+  sendKeys(keypressArrayCurrent, keypressArrayPrevious);
+
+  // Remember which keys were pressed so we can release them later
+  memcpy(keypressArrayPrevious, keypressArrayCurrent, sizeof(keypressArrayCurrent));
 }
 
 int scanKeyboard ( int keyArray [] [NCOLS] ) {
 
   int nKeysPressed = 0;
-  
+
   for (int row = 0; row < NROWS; row++) {
     //Serial.print("Scanning row ");
     //Serial.println(row);
-    
+
     // Select a pin to sink current
     pinMode(Rows[row], OUTPUT);
     digitalWrite(Rows[row], LOW);
     //delay(100);
-    
+
     for (int col = 0; col < NCOLS; col++) {
       //Serial.print("Scanning col. ");
       //Serial.println(col);
-      
+
       // Ignore any unmapped buttons
       if (keyScancode[row][col] != 0) {
-        
+
         // =1 for every button NOT pressed (pull-ups enabled)
         // =0 for every button PRESSED (signal shorted to row pin)
         // This value is inverted when saved
-        
+
         if ( digitalRead(Cols[col]) != HIGH ) {
           keyArray [row] [col] = 1;
           nKeysPressed++;
@@ -95,13 +83,13 @@ int scanKeyboard ( int keyArray [] [NCOLS] ) {
         }
       }
     }
-    
-    // Return pin to high-Z (inactive) mode 
+
+    // Return pin to high-Z (inactive) mode
     digitalWrite(Rows[row], HIGH);
     pinMode(Rows[row], INPUT);
-    
+
   }
-  
+
   return nKeysPressed;
 }
 
@@ -109,31 +97,87 @@ void sendKeys ( int pressedArray [] [NCOLS], int previousArray [] [NCOLS] ) {
 
   for ( int row = 0; row < NROWS; row++ ) {
     for ( int col = 0; col < NCOLS; col++ ) {
-      
+
       // Ignore any unmapped buttons
       if (keyScancode[row][col] != 0) {
-        
+
         if ( pressedArray[row][col] > previousArray[row][col] ) {
-            
-              if (SERIAL_ENABLED) {
-                Serial.print(keyScancode[row][col], HEX);
-                Serial.println(" pressed");
-              };
-            
-            Keyboard.press(keyScancode[row][col]);
-        } 
-        else if ( pressedArray[row][col] < previousArray[row][col] ) {
-            
+
+          if (SERIAL_ENABLED) {
+            Serial.print(keyScancode[row][col], HEX);
+            Serial.println(" pressed");
+          };
+
+          // Ugly hack to fix the Delete Key under Windows
+          // Is the backspace key pressed?
+          if ( row == 2 && col == 4) {
+
             if (SERIAL_ENABLED) {
-              Serial.print(keyScancode[row][col], HEX);
-              Serial.println(" released");
-            };
+              Serial.println("BACKSPACE PRESSED");
+              Serial.print("LSHIFT is ");
+              Serial.println(pressedArray[0][6]);
+              Serial.print("RSHIFT is ");
+              Serial.println(pressedArray[7][7]);
+            }
+
+            // Is any of the shift keys pressed?
+            if ( pressedArray[0][6] > 0 || pressedArray[7][7] > 0 ) {
+
+              if (SERIAL_ENABLED) Serial.println("Trying to send DELETE");
+
+              // Shift is pressed, send DELETE
+              Keyboard.press(KEY_DELETE);
+
+            } else {
+              // No Shift key pressed, send BACKSPACE
+              Keyboard.press(KEY_BACKSPACE);
+            }
             
+          } else if ( (row == 0 && col == 6) || (row == 7 && col == 7) ) {
+            // Special handling of the SHIFT keys
+            // Since we need to press SHIFT to send the KEY_DELETE, we need
+            // to make sure that we don't send the shift key as well UNLESS
+            // BOTH shift keys are pressed...
+
+            if (SERIAL_ENABLED) Serial.println("Handling SHIFT keys...");
+
+            if (pressedArray[2][4] > 0) {
+              // BACKSPACE/DELETE key is pressed
+              if ( (pressedArray[0][6] > 0) && (pressedArray[7][7] > 0) ) {
+                // Both shift keys are pressed, send the SHIFT keys
+                if (SERIAL_ENABLED) Serial.println("Both SHIFT pressed");
+                Keyboard.press(keyScancode[row][col]);
+              } // If only one was pressed, we don't want to send SHIFT.                
+            } else {
+              // Backspace / delete is not pressed
+              // Send the shift keys as usual
+              Keyboard.press(keyScancode[row][col]);
+            }
+          } else {
+            // "Normal" keypress, just send as is
+            Keyboard.press(keyScancode[row][col]);
+          }
+
+
+          // This handles the release of keys
+        } else if ( pressedArray[row][col] < previousArray[row][col] ) {
+
+          if (SERIAL_ENABLED) {
+            Serial.print(keyScancode[row][col], HEX);
+            Serial.println(" released");
+          };
+
+          // Make sure we release either BACKSPACE or DELETE; whatever was pressed
+          if ( row == 2 && col == 4) {
+            Keyboard.release(KEY_DELETE);
+            Keyboard.release(KEY_BACKSPACE);
+          } else {
             Keyboard.release(keyScancode[row][col]);
-            
+          }
+
         }
       }
-    }  
+    }
   }
-  
+
 }
--- Psion-Keyboard-Firmware/PsionKeymapUSB.h
+++ Psion-Keyboard-Firmware/PsionKeymapUSB.h
@@ -62,67 +62,9 @@
 #define KEY_UP         0xDA
 #define KEY_CTRL       0x80   // Right CTRL
 #define KEY_LSHIFT     0x81
-#define KEY_MENU       0x87   // Mapped as RIGHT_GUI
+#define KEY_MENU       0x82   // Mapped as Alt
 #define KEY_RSHIFT     0x85
-#define KEY_FN         0x82   // Mapped as Alt
-
-/* Old scancode map. This does not work anymore.
-#define KEY_A          0x04
-#define KEY_B          0x05
-#define KEY_C          0x06
-#define KEY_D          0x07
-#define KEY_E          0x08
-#define KEY_F          0x09
-#define KEY_G          0x0A
-#define KEY_H          0x0B
-#define KEY_I          0x0C
-#define KEY_J          0x0D
-#define KEY_K          0x0E
-#define KEY_L          0x0F
-#define KEY_M          0x10
-#define KEY_N          0x11
-#define KEY_O          0x12
-#define KEY_P          0x13
-#define KEY_Q          0x14
-#define KEY_R          0x15
-#define KEY_S          0x16
-#define KEY_T          0x17
-#define KEY_U          0x18
-#define KEY_V          0x19
-#define KEY_W          0x1A
-#define KEY_X          0x1B
-#define KEY_Y          0x1C
-#define KEY_Z          0x1D
-
-#define KEY_1          0x1E
-#define KEY_2          0x1F
-#define KEY_3          0x20
-#define KEY_4          0x21
-#define KEY_5          0x22
-#define KEY_6          0x23
-#define KEY_7          0x24
-#define KEY_8          0x25
-#define KEY_9          0x26
-#define KEY_0          0x27
-
-#define KEY_ENTER      0x28
-#define KEY_ESC        0x29
-#define KEY_BACKSPACE  0x2A
-#define KEY_TAB        0x2B
-#define KEY_SPACE      0x2C
-#define KEY_APOSTROPHE 0x35
-#define KEY_COMMA      0x36
-#define KEY_PERIOD     0x37
-#define KEY_RIGHT      0x4F
-#define KEY_LEFT       0x50
-#define KEY_DOWN       0x51
-#define KEY_UP         0x52
-#define KEY_CTRL       0xE0
-#define KEY_LSHIFT     0xE1
-#define KEY_MENU       0xE2  // Mapped as AltGr
-#define KEY_RSHIFT     0xE5
-#define KEY_FN         0xE6  // Mapped as Alt
-*/
+#define KEY_FN         KEY_RIGHT_ALT   // Mapped as AltGr
 
 static int keyScancode [NROWS] [NCOLS] = {
   { 0       ,KEY_SPACE ,KEY_UP ,KEY_COMMA ,KEY_LEFT      ,KEY_RIGHT      ,KEY_LSHIFT ,0          ,0        ,0      ,0        ,0       },
